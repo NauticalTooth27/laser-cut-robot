f8794c7ff902ba3d3a8083f49084fc3de0ae1901
NauticalTooth27 <ryan@polhemus.org>
2016-10-25 14:09:07 -0400

    Changed limbs as test

--- laserCutCad.groovy
+++ laserCutCad.groovy
@@ -154,6 +154,9 @@ return new ICadGenerator(){
 			}
 			
 		}
+
+		CSG testPiece = new Cube(20,40,20).movex(50).toCSG();
+		add(csg,moveDHValues(testPiece,dh),dh.getListener())
 		add(csg,moveDHValues(horn,dh),dh.getListener())
 
 		if(neck ==sourceLimb ){
